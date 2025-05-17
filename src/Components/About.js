60c96ff314c80f8ad95535436fed97024a2ff597
Utkarsh Raj <utkarshraj.1613@gmail.com>
2025-05-17 18:59:47 +0530

    deploy

---
+++ src/Components/About.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./About.css"
+
+function About() {
+  return (
+    <>
+      <div className='about-head'>About</div>
+      <div className='about'>
+        This is react resume builder made by Utkarsh Raj. This project is made by using complete React.js with poppular
+        react packages like react-redux, redux-persist, react-hook-form, react-router-dom, react-icons
+        and Nhost services like Postgres Database, GraphQl API and Storage has been used.
+
+        <div className={"mt-3"}>
+          This project was created as part of my personal portfolio to showcase my skills in React.js and modern web development. 
+         I am grateful for the open-source community and the creators of the packages and tools that made this project possible.
+        </div>
+        <a className={"mt-2 anchor-link"} href={"https://github.com/Amyx000/React-Resume-Builder"} target="_blank" rel="noopener noreferrer">Star our Github Repo</a>
+        </div>
+    </>
+  )
+}
+
+export default About
